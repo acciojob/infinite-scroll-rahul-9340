4b34521e680021b95da561b0179f398b15dd796e
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-07 15:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+let ol = document.getElementById("infi-list");
+for(let i=0; i<=10; i++)
+{
+let li = document.createElement("li")
 
+li.innerText= `list-item ${i}`
+
+ol.appendChild(li)
+}
+
+ol.addEventListener("scroll",()=>{
+	
+let ol = document.getElementById("infi-list");
+for(let i=0; i<=10; i++)
+{
+let li = document.createElement("li")
+
+li.innerText= `list-item ${i}`
+
+ol.appendChild(li)
+}
+})
